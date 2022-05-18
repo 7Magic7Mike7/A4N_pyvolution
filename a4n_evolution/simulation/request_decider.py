04ce1a25ebf219a8bd159ede90e5f20cda647ffc
7Magic7Mike7 <michael.artner@hotmail.de>
2022-05-18 16:32:54 +0200

    fixed bug causing condition to always be True and added extra condition to be True on the first call regardless of input

--- a4n_evolution/simulation/request_decider.py
+++ a4n_evolution/simulation/request_decider.py
@@ -1,12 +1,14 @@
 
 class RequestDecider:
     def __init__(self):
-        self.__counter = 0
+        self.__counter = -1
 
     def ever_x_steps(self, x: int):
         def should_request_new_data() -> bool:
+            if self.__counter < 0:  # first call should return True regardless of counter
+                self.__counter = x
             self.__counter += 1
-            if x >= self.__counter:
+            if x <= self.__counter:
                 self.__counter = 0
                 return True
             return False

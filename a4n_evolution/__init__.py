8d31e238e6d5c2b1e91c0c673573d2b5c328e5b8
7Magic7Mike7 <michael.artner@hotmail.de>
2022-07-04 18:35:06 +0200

    removed python test code

--- a4n_evolution/__init__.py
+++ a4n_evolution/__init__.py
@@ -3,21 +3,6 @@ from .interface import Interface
 
 
 def test():
-    nums_a = [12346, 47291, 98347, 48295, 80425, 17894]
-    nums_b = [48295, 80425, 17894, 12346, 47291, 98347]
-    nums_c = [99999, 11122, 34125, 90909, 10000, 47184]
-    nums_d = [12300, 41000, 72569, 39835, 70000, 14892]
-    factors = [2, 3, 5, 7, 11, 13, 17, 23, 29, 31, 37]
-    values = []
-    for nums in [nums_a, nums_b, nums_c, nums_d]:
-        value = 0
-        for i, num in enumerate(nums):
-            value += num * factors[i]
-        values.append(value / (10**5 * sum(factors[0:len(nums)])))
-
-    debug = True
-
-
     Interface.init()
     for i in range(0, 100):
         print(Interface.get_data())

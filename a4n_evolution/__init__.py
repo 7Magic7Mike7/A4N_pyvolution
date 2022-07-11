eacd3dbf555d1bf1e44b8e4518a363443927a418
7Magic7Mike7 <michael.artner@hotmail.de>
2022-07-11 19:04:10 +0200

    implemented loadable config

--- a4n_evolution/__init__.py
+++ a4n_evolution/__init__.py
@@ -23,8 +23,8 @@ def create_animated_plot():
         Interface.get_data()
 
     Interface.init()
-    anim = animation.FuncAnimation(plt.gcf(), get_data_wrapper, frames=Config.num_of_frames(),
-                                   interval=Config.interval(), repeat=False)
+    anim = animation.FuncAnimation(plt.gcf(), get_data_wrapper, frames=Config.instance().num_of_frames,
+                                   interval=Config.instance().interval, repeat=False)
     plt.show()
-    if Config.save_animation():
+    if Config.instance().save_animation:
         anim.save(os.path.join("data", "animated_plot.gif"))
